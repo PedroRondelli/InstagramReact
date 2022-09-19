e46d67cc48271578325fcc41e6b00817f336864d
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-09-19 13:28:32 -0300

    Trocar imagem usuário

--- src/Usuario.js
+++ src/Usuario.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 export default function Usuario(props) {
     const [usuario,setUsuario] = React.useState("Catana")
+    const [fotoUsuario,setFoto ] = React.useState("assets/img/catanacomics.svg") 
 
     function mudarNomeUsuario(props) {
         setUsuario(prompt("Nome de Usuário:"))
         
         
     }
+    function mudarFoto() {
+        setFoto(prompt("Link Foto de Usuário :"))
+    }
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" />
+            <img onClick={mudarFoto} src={fotoUsuario} />
             <div class="texto">
                 <strong>catanacomics</strong>
                 <span>
