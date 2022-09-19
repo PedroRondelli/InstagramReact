77d37c59c5ff3c0a7a9827fe0db8ab7a4422d22e
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-09-19 13:19:42 -0300

    Troca nome do usuário

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,12 +1,21 @@
+import React from 'react';
+
 export default function Usuario(props) {
+    const [usuario,setUsuario] = React.useState("Catana")
+
+    function mudarNomeUsuario(props) {
+        setUsuario(prompt("Nome de Usuário:"))
+        
+        
+    }
     return (
         <div class="usuario">
             <img src="assets/img/catanacomics.svg" />
             <div class="texto">
                 <strong>catanacomics</strong>
                 <span>
-                    Catana
-                    <ion-icon name="pencil"></ion-icon>
+                    {((usuario !== null && usuario !=="") ? usuario:"Catana")}
+                    <ion-icon onClick={mudarNomeUsuario} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
